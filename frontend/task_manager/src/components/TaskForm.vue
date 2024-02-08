7bf20f36b613436401032d9aac7ae8d00ba9be44
AwesomeY25 <jude.corciga@student.ateneo.edu>
2024-02-08 21:39:49 +0800

    Added Front End stuff

---
+++ frontend/task_manager/src/components/TaskForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="col-8 m-2 align-items-center">
+        <div class="row m-2">
+            <div class="col">
+                <label for="fname">First name:</label><br>
+                <input type="text" id="fname" name="fname"><br>
+            </div>
+        </div>
+        <div class="row m-2">
+            <div class="col">
+                <label for="lname">Last name:</label><br>
+                <input type="text" id="lname" name="lname">
+            </div>
+        </div>
+        <div class="row m-2">
+            <div class="col">
+                <label for="midinitial">Middle Initial:</label><br>
+                <input type="text" id="midinitial" name="midinitial">
+            </div>
+        </div>
+        <!-- Birthday -->
+        <div class="row m-2">
+            <div class="col">
+                <label for="dept">Choose a Department:</label><br>
+                <select id="dept" name="department">
+                <option value="1">Finance</option>
+                <option value="2">IT</option>
+                <option value="3">Human Resources</option>
+                <option value="4">Operations</option>
+                </select>
+            </div>
+        </div>
+        <input type="submit" value="Submit">
+    </div>
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'TaskForm',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
