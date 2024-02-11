9ab75d3a50a7658cac2fff3fa6b4b0e48f56f27c
Yra Corciga <jude.corciga@obf.ateneo.edu>
2024-02-11 12:47:59 +0800

    Added Func Req 1 (for Intern Account Creation)

--- backend/task_manager_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['user_id', 'user_username', 'user_fname', 'user_lname', 'user_minitial', 'user_email', 'user_password', 'user_account_type', 'timestamp']
--- frontend/task_manager_fe/README.md
+++ frontend/task_manager_fe/README.md
@@ -1,4 +1,4 @@
-# task_manager
+# task_manager_fe
 
 ## Project setup
 ```
